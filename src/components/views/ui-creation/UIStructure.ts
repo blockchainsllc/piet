f0c75ed8e775a7561d39cfa0093056290cdf8de5
Heiko Burkhardt <heiko@slock.it>
2019-07-15 10:32:31 +0200

    license

--- src/components/views/ui-creation/UIStructure.ts
+++ src/components/views/ui-creation/UIStructure.ts
@@ -1,6 +1,23 @@
 /**
+ *  This file is part of Piet.
+ *
+ *  Piet is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Piet is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Piet.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *  @author Heiko Burkhardt <heiko@slock.it>, Slock.it GmbH
  * 
  */
+
 
 export type SetUIStructure = (uiStructure: UIStructure) => void;
 export type AddRow = () => void;
--- src/solidity-handler/BlockchainConnector.ts
+++ src/solidity-handler/BlockchainConnector.ts
@@ -1,11 +1,21 @@
 /**
- * this file is part of bundesblock-voting
+ *  This file is part of Piet.
  *
- * it is subject to the terms and conditions defined in
- * the 'LICENSE' file, which is part of the repository.
+ *  Piet is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *
- * @author Heiko Burkhardt
- * @copyright 2018 by Slock.it GmbH
+ *  Piet is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Piet.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *  @author Heiko Burkhardt <heiko@slock.it>, Slock.it GmbH
+ * 
  */
 
 import * as Sol from './SolidityHandler';
